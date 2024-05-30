9a56ae0e148fa22556f20c10ca1b178abdd74d0c
kevinkliimask <kevinkliimask@gmail.com>
2024-05-30 20:36:13 +0300

    setup env and sqlalchemy

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config.from_pyfile("config.py")
+db = SQLAlchemy(app)
 
 @app.get("/")
 def home():
