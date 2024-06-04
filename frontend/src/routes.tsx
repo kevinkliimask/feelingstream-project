f8a89ad22cd73b307be1566bf6f2a7dc414f0e28
kevinkliimask <kevinkliimask@gmail.com>
2024-06-04 21:54:08 +0300

    improve frontend routing

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import Customers from "@/views/customers";
 import Interactions from "@/views/interactions";
@@ -6,8 +6,10 @@ import Interactions from "@/views/interactions";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route index path="/customers" element={<Customers />} />
-      <Route index path="/interactions" element={<Interactions />} />
+      <Route path="/" element={<Navigate to="/customers" />} />
+      <Route path="/customers" element={<Customers />} />
+      <Route path="/interactions" element={<Interactions />} />
+      <Route path="*" element={<Navigate to="/customers" />} />
     </Routes>
   );
 };
